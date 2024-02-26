6d1dfb72c4351b54f6abcd361f49bdc6c4cac83c
div-styl <gouhmada@gmail.com>
2024-02-26 21:49:09 +0100

    Fix variable name and update function call

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,19 +1,19 @@
-const fn = require('fs');
+const fs = require('fs');
 
 function countStudents(filename) {
-  const studenst = {};
+  const students = {};
   const fields = {};
   let length = 0;
 
   try {
-    const fileContents = fs.readFileSync(fileName, 'utf-8');
+    const fileContents = fs.readFileSync(filename, 'utf-8');
     const lines = fileContents.toString().split('\n');
 
     for (let i = 0; i < lines.length; i += 1) {
       if (lines[i]) {
         length += 1;
         const field = lines[i].toString().split(',');
-        if (Object.prototype.hasOwnProperty.call(studenst, field[3])) {
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
           students[field[3]].push(field[0]);
         } else {
           students[field[3]] = [field[0]];
