378be8a9e48f1ab417ff9afa5dcd3f5719bb89d7
div-styl <gouhmada@gmail.com>
2024-02-28 20:32:08 +0100

    Refactor sendPaymentRequestToApi function and add unit test

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+const { expect } = require("chai");
+const sinon = require("sinon");
+
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require("./4-payment");
+
+describe("sendPaymentRequestToApi function", () => {
+  const ConsoleUtilspy = sinon.spy(console, "log");
+
+  it("validate the usage of the Utils func", () => {
+    const calNumbstub = sinon.stub(Utils, "calculateNumber").returns(10);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(calNumbstub.calledWith("SUM", 100, 20)).to.be.true;
+    expect(calNumbstub.alwaysReturned(10)).to.be.true;
+    expect(ConsoleUtilspy.calledWith("The total is: 10")).to.be.true;
+
+    calNumbstub.restore();
+    ConsoleUtilspy.restore();
+  });
+});
