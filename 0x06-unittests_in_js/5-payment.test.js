779ad53fa6b9caa09067953abf354aaf70f7436a
div-styl <gouhmada@gmail.com>
2024-02-28 20:44:54 +0100

    Refactor sendPaymentRequestToApi function and add unit tests

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,27 @@
-#!/usr/bin/node
+const { expect } = require("chai");
+const sinon = require("sinon");
+
+const sendPaymentRequestToApi = require("./5-payment");
+
+describe("sendPaymentRequestToApi function", () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, "log");
+  });
+
+  afterEach(() => {
+    expect(consoleSpy.calledOnce).to.be.true;
+    consoleSpy.restore();
+  });
+
+  it("validate the usage of the Utils func", () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith("The total is: 120")).to.be.true;
+  });
+
+  it("validate the usage of the Utils func", () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith("The total is: 120")).to.be.true;
+  });
+});
