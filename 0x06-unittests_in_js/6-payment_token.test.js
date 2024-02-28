848f513e7edc342c1425e22fd9b3287de5b8d6c4
div-styl <gouhmada@gmail.com>
2024-02-28 20:49:42 +0100

    Refactor getPaymentTokenFromAPI function and add unit test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,18 @@
-#!/usr/bin/node
+const { expect } = require("chai");
+
+const getPaymentTokenFromAPI = require("./6-payment_token");
+
+describe("getPaymentTokenFromAPI function", () => {
+  it("validate the usage of the Utils func", () => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.include({
+          data: "Successful response from the API",
+        });
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
