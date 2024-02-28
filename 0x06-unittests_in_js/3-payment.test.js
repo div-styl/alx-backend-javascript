2bc4bfeec41516a23252906d1498ed717cbeadaa
div-styl <gouhmada@gmail.com>
2024-02-28 20:24:29 +0100

    Refactor payment module and add unit test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+const { expect } = require("chai");
+const sinon = require("sinon");
+
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require("./3-payment");
+
+describe("sendPaymentRequestToApi function", () => {
+  const Utilspy = sinon.spy(Utils, "calculateNumber");
+
+  it("validate the usage of the Utils func", () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(Utilspy.calledOnce).to.be.true;
+    expect(Utilspy.calledWith("SUM", 100, 20)).to.be.true;
+
+    Utilspy.restore();
+  });
+});
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,18 @@
-#!/usr/bin/node
+const Utils = {
+  calculateNumber: function (type, a, b) {
+    if (type === "SUM") {
+      return Math.round(a) + Math.round(b);
+    } else if (type === "SUBTRACT") {
+      return Math.round(a) - Math.round(b);
+    } else if (type === "DIVIDE") {
+      if (Math.round(b) === 0) {
+        return "Error";
+      } else {
+        return Math.round(a) / Math.round(b);
+      }
+    }
+    return 0;
+  },
+};
+
+module.exports = Utils;
