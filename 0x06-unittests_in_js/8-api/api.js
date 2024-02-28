f7280c595e99a4289edd877b7c5b31a6d855790d
div-styl <gouhmada@gmail.com>
2024-02-28 22:38:38 +0100

    Refactor API code and add unit tests

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const express = require("express");
+
+const app = express();
+const port = 7865;
+
+app.get("/", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Welcome to the payment system");
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+const { expect } = require("chai");
+const request = require("request");
+
+describe("Test of route", () => {
+  describe("GET /", () => {
+    it("returns status code 200", () => {
+      const op = {
+        url: "http://localhost:7865",
+        method: "GET",
+      };
+
+      request(op, function (err, res, body) {
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal("Welcome to the payment system");
+        done();
+      });
+    });
+  });
+});
